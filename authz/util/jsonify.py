8b5e478f7ac5e8d76fd586a7394778dafcfb84c5
saeedhosseini21 <saeedhosseini21@gmail.com>
2022-03-04 16:31:50 +0330

    updating jsonify error codes

--- authz/util/jsonify.py
+++ authz/util/jsonify.py
@@ -8,7 +8,17 @@ DEBUG_MSG_CODES = {
   "104": "Request Validation Failed",
   "105": "Empty Data Supplied",
   "106": "Resource Conflict",
-  "107": "Not Implemented."
+  "107": "Not Implemented.",
+  "108": "Resource Expired",
+  "109": "Bad Desired Status",
+  "110": "Token Encryption Error",
+  "111": "Resource Not Matched",
+  "112": "Header Not Specified",
+  "113": "Token Validation Error",
+  "114": "Invalid Token Data",
+  "115": "Controller Allowed Roles Not Found",
+  "116": "Resource Access Denied",
+  "117": "Role Not Found"
 }
 
 def jsonify(state={}, metadata={}, status=200, code=100, headers={}):
